d43dc77d43761fd4a88d7df2bc2f39273cb58475
elidata <eliedaat@gmail.com>
2022-07-31 14:36:43 +0300

    1:44

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home" ;
 import Category from './components/Category';
+import Search from './components/Search';
 import Pages from './pages/Pages';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Category />
-      <Pages />
+        <Search />
+        <Category />
+        <Pages />
       </BrowserRouter>
     </div>
   );
